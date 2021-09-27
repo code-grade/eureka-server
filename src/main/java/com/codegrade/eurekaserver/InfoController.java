84a0341ebd27a21239ea6a8ab56cc4449a381969
Thilina Lakshan <thilinalakshanmail@gmail.com>
2021-09-27 15:42:38 +0530

    add info endpoint to eureka server

--- src/main/java/com/codegrade/eurekaserver/InfoController.java
+++ src/main/java/com/codegrade/eurekaserver/InfoController.java
@@ -1,13 +1,31 @@
 package com.codegrade.eurekaserver;
 
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.shared.Application;
+import com.netflix.eureka.EurekaServerContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 public class InfoController {
 
+
+
     @GetMapping(path = "/info")
-    public String welcome() {
-        return "Hello World";
+    public Map<String, Object> welcome() {
+        var applications = EurekaServerContextHolder.getInstance()
+                .getServerContext().getRegistry()
+                .getApplications();
+
+        return applications.getRegisteredApplications().stream()
+                .collect(Collectors.toMap(
+                        Application::getName,
+                        app -> app.getInstances().stream()
+                                .map(InstanceInfo::getInstanceId).collect(Collectors.toList())
+                ));
     }
 }
