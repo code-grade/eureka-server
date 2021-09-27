6fa45f2161713eed42e544ce854120842d62f9c2
Thilina Lakshan <thilinalakshanmail@gmail.com>
2021-09-27 14:27:23 +0530

    remove postgres service from workflow

--- src/main/java/com/codegrade/eurekaserver/InfoController.java
+++ src/main/java/com/codegrade/eurekaserver/InfoController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class InfoController {
 
-    @GetMapping(path = "/")
+    @GetMapping(path = "/info")
     public String welcome() {
         return "Hello World";
     }
